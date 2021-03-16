6fae97dbb1101881e2f30aace19c76c0b70c1563
Ben Keeney <62183116+keensworth@users.noreply.github.com>
2021-03-16 17:52:28 -0500

    Add components

--- src/ecs/Components/World.java
+++ src/ecs/Components/World.java
@@ -1,32 +1,37 @@
 package ecs.Components;
 
 import ecs.Component;
+import ecs.Entity;
 import graphic.Mesh;
+import util.Container;
 
 public class World extends Component {
-    private Mesh[] world;
+    private Container[] world;
 
     public World(){
-        world = new Mesh[3];
+        world = new Container[5];
+        for (int i = 0; i < 5; i++){
+            world[i] = new Container();
+        }
     }
 
-    public Mesh getWorld(int index){
+    public Container getChunk(int index){
         return world[index];
     }
 
-    public Mesh[] getWorld(){
+    public Container[] getWorld(){
         return world;
     }
 
-    public void setWorld(int index, Mesh mesh){
-        world[index] = mesh;
+    public void setChunk(int index, Container chunk){
+        world[index] = chunk;
     }
 
-    public void setWorld(Mesh[] world){
+    public void setWorld(Container[] world){
         this.world = world;
     }
 
-    public World add(Mesh mesh){
+    public World add(){
         super.setLastWriteIndex(0);
         return this;
     }
