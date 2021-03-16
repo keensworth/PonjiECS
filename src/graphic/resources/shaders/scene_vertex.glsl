5a515439bd65b5b98e676f51003bbcb05d84a8b4
Ben Keeney <62183116+keensworth@users.noreply.github.com>
2021-03-16 17:56:40 -0500

    Relocate shaders

--- src/graphic/resources/shaders/scene_vertex.glsl
+++ src/graphic/resources/shaders/scene_vertex.glsl
@@ -2,6 +2,9 @@
 
 layout (location=0) in vec3 position;
 layout (location=1) in vec3 normal;
+layout (location=2) in vec2 texCoords;
+
+out vec2 outTexCoords;
 out vec3 outColor;
 flat out vec3 outNormal;
 out vec3 surfacePos;
@@ -9,6 +12,7 @@ out vec3 surfacePos;
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
+uniform mat4 rotationMatrix;
 uniform vec3 inColor;
 
 void main()
@@ -17,5 +21,6 @@ void main()
     surfacePos = vec3(modelMatrix * vec4(position,1.0f));
     //gl_Position = vec4(position, 1.0);
     outColor = inColor;
-    outNormal = normal;
+    outNormal = vec3(rotationMatrix * vec4(normal,0));
+    outTexCoords = texCoords;
 }
