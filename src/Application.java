202e55bd8f147c3ba0327c89c7ce880c4acb3c99
Ben Keeney <62183116+keensworth@users.noreply.github.com>
2021-03-16 17:59:52 -0500

    Add AssetManager and import assets

--- src/Application.java
+++ src/Application.java
@@ -4,6 +4,10 @@ import ecs.Components.*;
 import ecs.Events.Event;
 import ecs.Events.EventManager;
 import ecs.Systems.*;
+import graphic.AssetGroup;
+import graphic.AssetManager;
+import org.joml.Vector3f;
+import util.Container;
 
 public class Application {
     private ECS ecs;
@@ -13,6 +17,7 @@ public class Application {
     private BallSplit ballSplit;                      //object split quantity
     private Position position;                        //object position
     private Rotation rotation;                        //object rotation
+    private Scale scale;
     private Velocity velocity;                        //object velocity
     private NoCollide noCollide;                      //noCollide grouping
     private Health health;                            //object health
@@ -22,6 +27,9 @@ public class Application {
     private Shape polygon;
     private Points points;
     private World world;                              //stores terrain meshes
+    private Light light;
+    private ModelData modelData;
+    private MeshData meshData;
 
     //------Initialize Systems---------//
     private InputSys inputSys;                        //fetch input from GLFW
@@ -32,6 +40,8 @@ public class Application {
     private MovementSys movementSys;                  //update positions of moving entities
     private RenderSys renderSys;                      //color each object based on health, render to screen (paint screen)
 
+    private AssetManager assets;
+
     private final int WIDTH = 450;
     private final int HEIGHT = 900;
     protected final int RADIUS = 25;
@@ -39,35 +49,36 @@ public class Application {
     protected final float camDistance = (float) ((HEIGHT/2) * Math.sqrt(3));
 
     public Application() {
+
+        assets = new AssetManager(800);
+
         ecs = new ECS(WIDTH, HEIGHT);
-
-
         ecs.addEventManager(new EventManager(
                 new Event("example_event1"),
                 new Event("example_event2")
         ));
-
         ecs.addRenderer(
-                renderSys    = new RenderSys(ecs.width, ecs.height)
+                renderSys    = new RenderSys(ecs.width, ecs.height, assets)
         );
         ecs.setWindow(renderSys.getWindow());
-
         ecs.addComponent(
                 radius       = new Radius(),
                 ballSplit    = new BallSplit(),
                 position     = new Position(),
                 rotation     = new Rotation(),
+                scale        = new Scale(),
                 velocity     = new Velocity(),
                 noCollide    = new NoCollide(),
                 health       = new Health(),
                 camera       = new Camera(),
-                //render,
                 input        = new Input(),
                 polygon      = new Shape(),
                 points       = new Points(),
-                world        = new World()
+                world        = new World(),
+                light        = new Light(),
+                modelData    = new ModelData(),
+                meshData     = new MeshData()
         );
-
         ecs.addSystem(
                 inputSys      = new InputSys(),
                 controlSys    = new ControlSys(),
@@ -75,47 +86,46 @@ public class Application {
                 proceduralSys = new ProceduralSys(),
                 collisionSys  = new CollisionSys(),
                 pointSys      = new PointSys()
-
         );
 
         //initialize and run
+        importAssets();
         initScene(RADIUS);
         run();
+    }
+
+    private void importAssets(){
+        assets.importAssets("C:\\Users\\Sargy\\IdeaProjects\\PonjiECS\\src\\graphic\\resources\\models");
+        Container<AssetGroup> assetGroups = assets.getAssetGroups();
+        AssetGroup city = assetGroups.get(0);
+        AssetGroup western = assetGroups.get(3);
+        AssetGroup nature = assetGroups.get(1);
+
+        city.setRotation(3.1315f/2, new Vector3f(1,0,0));
+        city.setScale(33);
+        western.setRotation(3.1315f/2, new Vector3f(1,0,0));
+        western.setScale(0.75f);
+        nature.setScale(75);
     }
 
     private void initScene(int eRadius){
         ecs.createEntity(
                 position.add(new float[]{0,0,camDistance}),
-                rotation.add(new float[]{0,0,0}),
+                rotation.add(new float[]{-PI/8,0,0}),
                 camera.add()
         );
 
-        for (int i = 0; i < 8; i ++){
+        /*
+        for (int i = 0; i < 4; i ++){
             ecs.createEntity(
-                    position.add(new float[]{0,-300 + i*100, 0}),
+                    position.add(new float[]{0,-150 + i*150, 0}),
                     health.add((i+1)*12),
                     radius.add(eRadius),
-                    ballSplit.add(2)
+                    ballSplit.add(2),
+                    light.add(Light.POINT_LIGHT)
             );
         }
-
-        for (int i = 0; i < 8; i ++){
-            ecs.createEntity(
-                    position.add(new float[]{75,-350 + i*100, 0}),
-                    health.add((i+1)*12),
-                    radius.add(eRadius),
-                    ballSplit.add(2)
-            );
-        }
-
-        for (int i = 0; i < 8; i ++){
-            ecs.createEntity(
-                    position.add(new float[]{-75,-350 + i*100, 0}),
-                    health.add((i+1)*12),
-                    radius.add(eRadius),
-                    ballSplit.add(2)
-            );
-        }
+         */
     }
 
     //Main loop
@@ -126,6 +136,7 @@ public class Application {
             start = System.nanoTime();
 
             update((float)1/60);
+            System.out.println("UPDATED");
 
             end = System.nanoTime();
             while (end-start < 16000000){
