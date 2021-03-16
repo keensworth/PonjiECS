dd981241f95f1d28529f841ed6a4bc2af97723e2
Ben Keeney <62183116+keensworth@users.noreply.github.com>
2021-03-16 17:54:36 -0500

    Adjust movement and camera tracking

--- src/ecs/Systems/MovementSys.java
+++ src/ecs/Systems/MovementSys.java
@@ -4,6 +4,7 @@ import ecs.Entity;
 import ecs.System;
 import ecs.Components.*;
 import util.ComponentMask;
+import util.Container;
 import util.ETree.EntNode;
 
 public class MovementSys extends System {
@@ -18,12 +19,15 @@ public class MovementSys extends System {
     public Class update(float dt, EntNode entityTree, ComponentMask componentMask, boolean entityChange) {
         java.lang.System.out.println("Updating MovementSys");
         Entity[] entities = getEntities(entityTree);
+        Entity[] camera = getEntities(entityTree, new Class[]{Camera.class});
 
         int[] positionIndices = getComponentIndices(Position.class, entities, componentMask);
         int[] velocityIndices = getComponentIndices(Velocity.class, entities, componentMask);
 
         position = (Position) componentMask.getComponent(Position.class);
         velocity = (Velocity) componentMask.getComponent(Velocity.class);
+
+        float highestBallYPos = 0;
 
         if (velocityIndices.length==0){
             //return ControlSys.class;
@@ -43,11 +47,19 @@ public class MovementSys extends System {
                 float xPosition = positionVec[0] + xVelocity*dt;
                 float yPosition = positionVec[1] + yVelocity*dt;
 
+                if (yPosition > highestBallYPos && yPosition > 0)
+                    highestBallYPos = yPosition;
+
                 position.setXPos(positionIndex, xPosition);
                 position.setYPos(positionIndex, yPosition);
             }
+
+            position.setYPos(position.getEntityIndex(camera[0].getEntityId()),highestBallYPos);
+
             return null;
         }
+
+
     }
 
     @Override
